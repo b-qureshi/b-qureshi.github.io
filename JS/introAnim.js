9a93122910f2361dc7a4b8720454f42c69d775ef
Bilal <b.qureshi1@outlook.com>
2021-07-12 18:06:32 +0100

    Intro anim played once

--- JS/introAnim.js
+++ JS/introAnim.js
@@ -1,22 +1,36 @@
+// Main page delay
+delay = '^3300';
+fadeInAnim = document.documentElement;
+
 // Main Intro animation
-var typed = new Typed(".animate1", {
-    strings: [
-        "^300 I'm Bilal Qureshi.",
-    ],
-    typeSpeed: 60,
-    loop: false,
-    showCursor: true
-  });
+if (window.sessionStorage.getItem('loaded') != 'true'){
+  var typed = new Typed(".animate1", {
+      strings: [
+          "^300 I'm Bilal Qureshi.",
+      ],
+      typeSpeed: 60,
+      loop: false,
+      showCursor: true
+    });
+  window.sessionStorage.setItem('loaded', 'true')
+
+  delay = '^3300';
+  fadeInAnim.style.setProperty('--delay-fade', '3.4s')
+} else {
+  delay = '';
+  fadeInAnim.style.setProperty('--delay-fade', '0s');
+  document.getElementById("introduction").style.opacity = "0"; 
+}
 
   // Sub heading animation
-  var typed2 = new Typed(".animate2", {
-    strings: [
-        "^3300 about me",
-    ],
-    typeSpeed: 60,
-    loop: false,
-    showCursor: false
-  });
+var typed2 = new Typed(".animate2", {
+  strings: [
+      delay +" about me",
+  ],
+  typeSpeed: 60,
+  loop: false,
+  showCursor: false
+});
 
 
 
